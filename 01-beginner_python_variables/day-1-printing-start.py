3a6fb8e38fcc34cb9aa3242ed44fe9744997e1c5
Anggi Budi Kurniawan <anggibudik@users.noreply.github.com>
2020-11-13 05:08:05 +0900

    Lesson 1 goal
    Create a band name generator based on users' input about their childhood city and pet name.

---
+++ 01-beginner_python_variables/day-1-printing-start.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Hey! Here's a cool band name you could use: {} {}".format(city,pet))
+# or
+# print("Hey! Here's a cool band name you could use: " + city + " " + pet)
