8555707f964494bc5401010d9a23223931ea45af
Anggi Budi Kurniawan <anggibudik@users.noreply.github.com>
2020-11-14 05:17:19 +0900

    Update d1-band-name-generator.py

--- 01-beginner_level/d1-band-name-generator.py
+++ 01-beginner_level/d1-band-name-generator.py
@@ -5,7 +5,7 @@
     100 Days of Code - Udemy Lecture by Dr. Angela Yu
 '''
 
-print("Welcome to the band name generator! (by AnggiBK)"
+print("Welcome to the band name generator! (by AnggiBK)")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Hey! Here's a cool band name you could use: {} {}".format(city,pet))
